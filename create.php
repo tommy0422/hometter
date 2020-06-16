eb09e592069b0545b063b50598a5d7c48ab76c00
tommy0422 <tommy.fun39@gmail.com>
2020-06-16 09:23:01 +0900

    テーブル結合成功

--- create.php
+++ create.php
@@ -6,11 +6,43 @@ session_start();
 include('functions.php');
 check_session_id();
 
+//DB接続の設定
+$pdo = connect_to_db();
+
+//ユーザーIDの定義
+$user_id = $_SESSION["user_id"];
+// var_dump($user_id);
+// exit();
+
+//データの取得
+$sql = 'SELECT * FROM users_table WHERE user_id = :user_id';
+
+// SQL準備&実行
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
+$status = $stmt->execute();
+
+if ($status == false) {
+    // SQL実行に失敗した場合はここでエラーを出力し，以降の処理を中止する
+    $error = $stmt->errorInfo();
+    echo json_encode(["error_msg" => "{$error[2]}"]);
+    exit();
+} else {
+    // 正常にSQLが実行された場合は指定の11レコードを取得
+    // fetch()関数でSQLで取得したレコードを取得できる
+    $record = $stmt->fetch(PDO::FETCH_ASSOC);
+    // var_dump($record);
+    // exit();
+}
+
+//ユーザーIDの再定義
+$user_id = $record['id'];
+// var_dump($user_id);
+// exit();
+
 //データ受け取り
 $player = $_POST['player'];
 $text = $_POST['text'];
-$from_player = $_SESSION['user_id'];
-
 
 // 項目入力のチェック
 // 値が存在しないor空で送信されてきた場合はNGにする
@@ -23,15 +55,12 @@ if (
     exit();
 }
 
-//DB接続
-$pdo = connect_to_db();
-
 // データ登録SQL作成
-$sql = 'INSERT INTO post(id, from_player, player, text, created_at) VALUES(NULL, :from_player ,:player, :text, sysdate())';
+$sql = 'INSERT INTO post(id, from_player, player, text, created_at) VALUES(NULL, :user_id ,:player, :text, sysdate())';
 
 // SQL準備&実行
 $stmt = $pdo->prepare($sql);
-$stmt->bindValue(':from_player', $from_player, PDO::PARAM_STR);
+$stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
 $stmt->bindValue(':player', $player, PDO::PARAM_STR);
 $stmt->bindValue(':text', $text, PDO::PARAM_STR);
 $status = $stmt->execute();
--- home.php
+++ home.php
@@ -6,39 +6,8 @@ check_session_id();
 //DB接続の設定
 $pdo = connect_to_db();
 
-//ユーザーIDの定義
-$user_id = $_SESSION["user_id"];
-// var_dump($user_id);
-// exit();
-
-//データの取得
-$sql = 'SELECT * FROM users_table WHERE user_id = :user_id';
-
-// SQL準備&実行
-$stmt = $pdo->prepare($sql);
-$stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
-$status = $stmt->execute();
-
-if ($status == false) {
-    // SQL実行に失敗した場合はここでエラーを出力し，以降の処理を中止する
-    $error = $stmt->errorInfo();
-    echo json_encode(["error_msg" => "{$error[2]}"]);
-    exit();
-} else {
-    // 正常にSQLが実行された場合は指定の11レコードを取得
-    // fetch()関数でSQLで取得したレコードを取得できる
-    $record = $stmt->fetch(PDO::FETCH_ASSOC);
-    // var_dump($record);
-    // exit();
-}
-
-//ユーザーIDの再定義
-$user_id = $record['user_id'];
-// var_dump($user_id);
-// exit();
-
 // データ取得SQL作成
-$sql = 'SELECT * FROM post';
+$sql = 'SELECT * FROM post LEFT OUTER JOIN users_table ON post.from_player = users_table.id;';
 
 // SQL準備&実行
 $stmt = $pdo->prepare($sql);
@@ -54,17 +23,20 @@ if ($status == false) {
     // 正常にSQLが実行された場合は入力ページファイルに移動し，入力ページの処理を実行する
     // fetchAll()関数でSQLで取得したレコードを配列で取得できる
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);  // データの出力用変数（初期値は空文字）を設定
+    // var_dump($result);
+    // exit();
     $output = "";
     // <tr><td>deadline</td><td>todo</td><tr>の形になるようにforeachで順番に$outputへデータを追加
     // `.=`は後ろに文字列を追加する，の意味
     foreach ($result as $record) {
         $output .= "<div class=post_box>";
-        $output .= "<h4>From {$user_id} <br> ---> {$record["player"]}</h4>";
+        $output .= "<h4>From {$record["user_id"]} <br> ---> {$record["player"]}</h4>";
         $output .= "<td>{$record["text"]}</td>";
         $output .= "</div>";
         $output .= "<hr>";
     }
-
+    // var_dump($record);
+    // exit();
     // $valueの参照を解除する．解除しないと，再度foreachした場合に最初からループしない
     // 今回は以降foreachしないので影響なし
     unset($record);
