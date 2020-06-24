74ecc9217898cb95738483e01c0323561bef0d90
tommy0422 <tommy.fun39@gmail.com>
2020-06-25 03:14:19 +0900

    承認欲求ポイント機能追加

--- setting.php
+++ setting.php
@@ -7,8 +7,6 @@ check_session_id();
 $pdo = connect_to_db();
 
 $user_id = $_SESSION["user_id"];
-// var_dump($user_id);
-// exit();
 
 // データ取得SQL作成
 $sql = 'SELECT * FROM users_table WHERE user_id = :user_id';
@@ -35,7 +33,29 @@ if ($record["image"] != NULL) {
     $user_icon = "img/no_image.png";
 }
 
+//ここから承認欲求ポイント
+$sql = "SELECT COUNT(player) FROM post LEFT OUTER JOIN  users_table ON post.player = users_table.id WHERE user_id = :user_id";
 
+//sql実行
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
+$status = $stmt->execute();
+
+// データ登録処理後
+if ($status == false) {
+    // SQL実行に失敗した場合はここでエラーを出力し，以降の処理を中止する
+    $error = $stmt->errorInfo();
+    echo json_encode(["error_msg" => "{$error[2]}"]);
+    exit();
+} else {
+    $record2 = $stmt->fetch(PDO::FETCH_ASSOC);
+    //     var_dump($record);
+    //     exit();
+}
+
+$point = $record2;
+// var_dump($point);
+// exit();
 ?>
 
 <!DOCTYPE html>
@@ -45,6 +65,7 @@ if ($record["image"] != NULL) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Walter+Turncoat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
     <link rel="stylesheet" href="css/setting.css">
     <title>設定画面</title>
 </head>
@@ -81,6 +102,10 @@ if ($record["image"] != NULL) {
             </div>
             <p>※変更するとログイン画面に戻ります.</p>
             <input type="hidden" name="id" value="<?= $record["id"] ?>">
+        </fieldset>
+        <fieldset class="point">
+            <legend>Desire for approval</legend>
+            <p id="point"><?= $point["COUNT(player)"] ?> point</p>
         </fieldset>
     </form>
 
